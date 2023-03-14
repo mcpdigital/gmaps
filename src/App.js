0c7252482b45e81b48d42e1309828e6791500c09
mcpdigital <internet@mcpdigital.com>
2023-03-14 15:51:59 -0300

    gmap

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import MapContainer from './MapContainer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Quick Find Locations</h1>
+      <h3>Write the location with details</h3>
+            <MapContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MapContainer.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import axios from 'axios';
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+const MapContainer = ({ google }) => {
+  const [location, setLocation] = useState('');
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+
+  const handleLocationChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const handleLocationSubmit = (e) => {
+    e.preventDefault();
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=AIzaSyCJCVbHoAaaa1WTL0vTdiqSOLfWgyXYIho`)
+      .then((response) => {
+        const { lat, lng } = response.data.results[0].geometry.location;
+        setLat(lat);
+        setLng(lng);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleLocationSubmit}>
+        <input type="text" value={location} onChange={handleLocationChange} />
+        <button type="submit">Search</button>
+      </form>
+      
+      <p>
+        Latitude: {lat}
+      </p>
+      <p>
+        Longitude: {lng}
+      </p>
+      {lat && lng && (
+        <Map
+          google={google}
+          zoom={14}
+          style={mapStyles}
+          initialCenter={{ lat, lng }}
+          center={{ lat, lng }}
+        >
+          <Marker position={{ lat, lng }} />
+        </Map>
+      )}
+    </div>
+  );
+};
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCJCVbHoAaaa1WTL0vTdiqSOLfWgyXYIho'
+})(MapContainer);
